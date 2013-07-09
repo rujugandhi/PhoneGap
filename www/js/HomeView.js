5a88d107f0440f6982312e161365176ba2c292db
Asad Palekar <Asad Palekar>
2013-07-09 18:32:26 +0530

    Adding View model in UI

---
+++ www/js/HomeView.js
@@ -0,0 +1,22 @@
+﻿var HomeView = function(dal,applicationDataModel) {
+    this.dal = dal;
+    this.adm = applicationDataModel;
+    this.el = null;
+    this.Initialize = function () {
+        //Defining a div wrapper for a view to display elements and bind events on. 
+        this.el = $('<div/>');
+        //Bind Events here. 
+    };
+    this.Render = function() {
+        //Call the dal (Data Access Layer) to get expenses and render them into the div wrapper
+        this.dal.Read(this.adm).done(function(exp) {
+            //When read is done. 
+            var renderedText = HomeView.ExpenseListTemplate(exp);
+            return renderedText;
+        });
+    };
+    this.Initialize();
+};
+
+
+HomeView.ExpenseListTemplate = Handlebars.compile($('#ExpenseListTemplate').html());
---
+++ www/js/NewExpenseView.js
@@ -0,0 +1,28 @@
+﻿var NewExpenseView = function(dac, adm) {
+    this.dac = dac;
+    this.adm = adm;
+    this.el = null;
+    this.Initiliaze = function() {
+        var self = this;
+        this.el = $('<div/>');
+        //Bind events. 
+        this.el.on('click', '#btnSave', function() {
+                var name = $('#txtName').val();
+                var amount = $('#txtAmount').val();
+                var cat = $('#txtCategory').val();
+                self.adm.AddExpense(name, amount, cat);
+                self.dac.Save(self.adm).done(function() {
+                $('li#navCurrentExpenses').click();
+            });
+        });
+    };
+
+    this.Render = function() {
+        this.el.html(NewExpenseView.NewExpenseTemplate());
+        return this.el;
+    };
+
+    this.Initiliaze();
+};
+
+NewExpenseView.NewExpenseTemplate = Handlebars.compile($('#newExpenseTemplate').html());
